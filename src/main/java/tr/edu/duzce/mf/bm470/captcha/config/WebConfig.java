69aaa0430178dabbd6d3f8bd4ee504c87a1d5af6
VolkanCan <volkan.baskoy@alpakyazilim.com>
2021-05-16 19:59:32 +0300

    fsfsf

--- src/main/java/tr/edu/duzce/mf/bm470/captcha/config/WebConfig.java
+++ src/main/java/tr/edu/duzce/mf/bm470/captcha/config/WebConfig.java
@@ -40,6 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+        registry.addInterceptor(new LoggerInterceptor())
+        .excludePathPatterns("/resources/**");
     }
 }
